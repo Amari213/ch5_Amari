0d7a49e3429e219d0bd80f6e287b79c7467e411c
amari <awilliams213@ggc.edu>
2024-03-26 23:17:13 -0400

    initial commit

--- src/SumNum.java
+++ src/SumNum.java
@@ -1,10 +1,19 @@
 public class SumNum {
 
+    private SumNum() {
+    }
+
+    public SumNum() {
+
+    }
+
     // main method is a static method
     public static int sum(int number1 + number2) {
         return 0;
     }
 
-        
 
+    public static SumNum() {
+        return new SumNum();
     }
+}
